47f0d1e35d7cc942968c611b7576608dbc159462
Leonardo Lopes <leonardo.lopes@camisariafascynios.com.br>
2020-04-12 22:52:34 -0300

    Change package import

--- rest-spring-boot/src/main/java/br/com/erudio/services/PersonService.java
+++ rest-spring-boot/src/main/java/br/com/erudio/services/PersonService.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.erudio.data.models.Person;
 import br.com.erudio.exception.ResourceNotFoundException;
-import br.com.erudio.models.Person;
 import br.com.erudio.repositories.PersonRepository;
 
 @Service
